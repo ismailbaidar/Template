d3c7a03281f73ff91dd5cb43923d1c2927a26561
Ismail <ismail.baidar198@gmail.com>
2024-01-17 15:58:37 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import "../assets/styles/app.css"
+export default function Navbar() {
+  return (
+    <nav className="p-4   shadow-md flex align-center justify-between">
+      <span>LOGO</span>
+      <ul className="nav-links flex gap-3">
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+        <li className="nav-link hover:text-red-700 cursor-pointer">route</li>
+      </ul>
+    </nav>
+  )
+}
