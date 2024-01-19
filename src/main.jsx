0966fc72b833cf79b28934acd57345b0179e2b6e
Ismail <ismail.baidar198@gmail.com>
2024-01-19 10:09:40 +0100

    ab

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,7 @@ import App from "./App"
 import { BrowserRouter } from "react-router-dom"
 const root = ReactDOM.createRoot(document.querySelector("#root"))
 root.render(
-<<<<<<< HEAD
   <BrowserRouter>
     <App />
   </BrowserRouter>
-=======
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-
->>>>>>> af18eb0d4921ea71f9b06e6da0647c5118ff1c3a
 )
