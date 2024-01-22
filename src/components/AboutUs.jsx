69497957a4f92fedb0d730fda4323503de51a3f6
Mustapha Roudani <roudani.mustapha0@gmail.com>
2024-01-22 22:38:59 +0100

    push page <About Us>

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -6,7 +6,7 @@ function AboutUs() {
     <>
       <section className='section'>
         <div className='titel'>
-          <h2>About Us</h2>
+          <h1>About Us</h1>
         </div>
         <div className='container'>
           <div className='card-cont'>
@@ -14,7 +14,7 @@ function AboutUs() {
 
             <div className='image-container'>
 
-              <img src="../Images/Blue.jpg" alt="" />
+              <img src="../Images/Blue.png" alt="" />
 
             </div>
             <div className='text-contain'>
@@ -47,9 +47,40 @@ function AboutUs() {
             </div>
 
           </div>
+          <div className='card-cont'>
+
+
+            <div className='image-container'>
+
+              <img src="../Images/Blue.jpg" alt="" />
+
+            </div>
+            <div className='text-contain'>
+              <div className='text-header'>
+                <h4>Moroccan Microsoft Community</h4>
+              </div>
+              <div className='text-Lorem'>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, magni! Quis recusandae officia expedita quisquam earum? Amet temporibus
+                qui vitae sequi dolores eligendi, repellendus id culpa inventore officiis enim! Deserunt.
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+        <div className='text-miss'>
+          <div className='titel'>
+              <h1>Our Values</h1>
+          </div>
+          <div className='description'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi a possimus quisquam quaerat pariatur, et, exercitationem
+              modi facilis odio vero, ex Lorem ipsum dolor sit amet consectetur adipisicing elit. Est odio totam in, esse unde illum rerum dolore sed? Aspernatur
+            </p>
+          </div>
 
         </div>
       </section>
+
     </>
   )
 }
