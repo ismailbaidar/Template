9b8589d03cc2189f7b47f9805db382a0b84aeb07
Mustapha Roudani <roudani.mustapha0@gmail.com>
2024-01-24 09:17:18 +0100

    Update speakers page

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -3,6 +3,95 @@ import '../assets/styles/speakers.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faLinkedin, faInstagram, faFacebook, } from "@fortawesome/free-brands-svg-icons"
 import {faEnvelope} from "@fortawesome/free-solid-svg-icons"
+const speakersData = [
+  {
+    name: 'Said WAHID',
+    role: 'Consultant Microsoft Cloud AZURE .Net & Office 365',
+    image: '../Images/said_wahid.jpg',
+    social: {
+      facebook: 'https://www.facebook.com/saidwahidmaroc',
+      linkedin: 'https://www.linkedin.com/in/said-wahid-a0927715/?originalSubdomain=ma',
+      instagram: 'https://www.instagram.com/saidwahidmaroc/?hl=en',
+    },
+  },
+  {
+    name: 'Hassan FADILI',
+    role: 'MVP Microsoft Cloud AZURE .Net & Office 365',
+    image: '../Images/hassan_fadili.jpg',
+    social: {
+      facebook: 'https://www.facebook.com/hassan.fadili.948',
+      linkedin: 'https://www.linkedin.com/in/hassan-fadili-3836422/?originalSubdomain=nl',
+      instagram: 'https://www.instagram.com/hassanfad11/?hl=en',
+    },
+  },
+  {
+    name: 'Youssef CHIGANE',
+    role: 'DATA & AI Consultant',
+    image: '../Images/youssef_chigane.jpg',
+    social: {
+      linkedin: 'https://www.linkedin.com/in/youssef-chigane/',
+    },
+  },
+  {
+    name: 'Mohssine MASAAF',
+    role: 'Consultant .NET Technical Lead',
+    image: '../Images/mohssine_masaaf.jpg',
+    social: {
+      facebook: 'https://www.facebook.com/MohssineRealReal',
+      linkedin: 'https://www.linkedin.com/in/mohssinemasaaf/',
+      instagram: 'https://www.facebook.com/saidwahidmaroc',
+    },
+  },
+  {
+    name: 'Outman BAZZAZ',
+    role: 'DATA & Business Intelligence Expert',
+    image: '../Images/outman_bazzaz.jpg',
+    social: {
+      linkedin: 'https://www.linkedin.com/in/outmanbaz/',
+    },
+  },
+  {
+    name: 'Anas BELABBES',
+    role: 'MVP Microsoft',
+    image: '../Images/anas_belabbes.jpg',
+    social: {
+      facebook: 'https://www.facebook.com/anas.belabbes',
+      linkedin: 'https://www.linkedin.com/in/anasbelabbes/',
+      instagram: 'https://www.facebook.com/saidwahidmaroc',
+    },
+  },
+ 
+];
+
+const Speaker = ({ name, role, image, social }) => (
+  <div className='cards'>
+    <div className='tic-tac'>
+      <img src={image} width={200} alt={name} />
+    </div>
+    <div className='info-said'>
+      <h5>{name}</h5>
+      <p>{role}</p>
+    </div>
+    <div className='nav-Social'>
+      {Object.entries(social).map(([platform, link]) => (
+        <div key={platform} className={platform}>
+          <a href={link}>
+            {platform === 'email' ? (
+              <FontAwesomeIcon icon={faEnvelope} />
+            ) : platform === 'facebook' ? (
+              <FontAwesomeIcon icon={faFacebook} />
+            ) : platform === 'linkedin' ? (
+              <FontAwesomeIcon icon={faLinkedin} />
+            ) : platform === 'instagram' ? (
+              <FontAwesomeIcon icon={faInstagram} />
+            ) : null}
+          </a>
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
 
 function speakers() {
   return (
@@ -14,225 +103,9 @@ function speakers() {
           </div>
 
           <div className='card-container'>
-            <div className='cards'>
-              <div className='tic-tac'>
-                <img src="../Images/said_wahid.jpg" width={200} alt="" />
-              </div>
-              <div className='info-said'>
-                <h5>Said WAHID</h5>
-                <p>Consultant Microsoft Cloud AZURE  .Net & Office 365</p>
-              </div>
-
-
-              <div className='nav-Social'>
-                <div className='facebook'>
-                  <a href="https://www.facebook.com/saidwahidmaroc">
-                    <FontAwesomeIcon icon={faFacebook}  />
-                  </a>
-                </div>
-                <div className='linkedin'>
-                  <a href="https://www.linkedin.com/in/said-wahid-a0927715/?originalSubdomain=ma">
-                    <FontAwesomeIcon icon={faLinkedin}  />
-                  </a>
-                </div>
-                <div className='instagram'>
-                  <a href="https://www.instagram.com/saidwahidmaroc/?hl=en">
-                    <FontAwesomeIcon icon={faInstagram}  />
-                  </a>
-                </div>
-                <div className='Email'>
-                  <a href="#">
-                    <FontAwesomeIcon icon={faEnvelope}  />
-                  </a>
-                </div>
-              </div>
-
-            </div>
-
-            <div className='cards'>
-              <div className='tic-tac'>
-                <img src="../Images/hassan_fadili.jpg" width={200} alt="" />
-              </div>
-              <div className='info-said'>
-                <h5>Hassan FADILI</h5>
-                <p>MVP Microsoft Cloud AZURE  .Net & Office 365</p>
-              </div>
-
-
-              <div className='nav-Social'>
-                <div className='facebook'>
-                  <a href="https://www.facebook.com/hassan.fadili.948">
-                    <FontAwesomeIcon icon={faFacebook}  />
-                  </a>
-                </div>
-                <div className='linkedin'>
-                  <a href="https://www.linkedin.com/in/hassan-fadili-3836422/?originalSubdomain=nl">
-                    <FontAwesomeIcon icon={faLinkedin}  />
-                  </a>
-                </div>
-                <div className='instagram'>
-                  <a href="https://www.instagram.com/hassanfad11/?hl=en">
-                    <FontAwesomeIcon icon={faInstagram}  />
-                  </a>
-                </div>
-                <div className='Email'>
-                  <a href="#">
-                    <FontAwesomeIcon icon={faEnvelope}  />
-                  </a>
-                </div>
-
-
-
-              </div>
-
-            </div>
-            <div className='cards'>
-              <div className='tic-tac'>
-                <img src="../Images/youssef_chigane.jpg" width={200} alt="" />
-              </div>
-              <div className='info-said'>
-                <h5>Youssef CHIGANE</h5>
-                <p>DATA & AI Consultant</p>
-              </div>
-
-
-              <div className='nav-Social'>
-                <div className='facebook'>
-                  <a href="#">
-                    <FontAwesomeIcon icon={faFacebook}  />
-                  </a>
-                </div>
-                <div className='linkedin'>
-                  <a href="https://www.linkedin.com/in/youssef-chigane/">
-                    <FontAwesomeIcon icon={faLinkedin}  />
-                  </a>
-                </div>
-                <div className='instagram'>
-                  <a href="#">
-                    <FontAwesomeIcon icon={faInstagram}  />
-                  </a>
-                </div>
-                <div className='Email'>
-                  <a href="#">
-                    <FontAwesomeIcon icon={faEnvelope}  />
-                  </a>
-                </div>
-
-
-
-              </div>
-
-            </div>
-            <div className='cards'>
-              <div className='tic-tac'>
-                <img src="../Images/mohssine_masaaf.jpg" width={200} alt="" />
-              </div>
-              <div className='info-said'>
-                <h5>Mohssine MASAAF</h5>
-                <p>Consultant .NET Technical Lead  </p>
-              </div>
-
-
-              <div className='nav-Social'>
-                <div className='facebook'>
-                  <a href="https://www.facebook.com/MohssineRealReal">
-                    <FontAwesomeIcon icon={faFacebook}  />
-                  </a>
-                </div>
-                <div className='linkedin'>
-                  <a href="https://www.linkedin.com/in/mohssinemasaaf/">
-                    <FontAwesomeIcon icon={faLinkedin}  />
-                  </a>
-                </div>
-                <div className='instagram'>
-                  <a href="https://www.facebook.com/saidwahidmaroc">
-                    <FontAwesomeIcon icon={faInstagram}  />
-                  </a>
-                </div>
-                <div className='Email'>
-                  <a href="#">
-                    <FontAwesomeIcon icon={faEnvelope}  />
-                  </a>
-                </div>
-
-
-
-              </div>
-
-            </div><div className='cards'>
-              <div className='tic-tac'>
-                <img src="../Images/outman_bazzaz.jpg" width={200} alt="" />
-              </div>
-              <div className='info-said'>
-                <h5>Outman BAZZAZ</h5>
-                <p>DATA & Business Intelligence Expert</p>
-              </div>
-
-
-              <div className='nav-Social'>
-                <div className='facebook'>
-                  <a href="#">
-                    <FontAwesomeIcon icon={faFacebook}  />
-                  </a>
-                </div>
-                <div className='linkedin'>
-                  <a href="https://www.linkedin.com/in/outmanbaz/">
-                    <FontAwesomeIcon icon={faLinkedin}  />
-                  </a>
-                </div>
-                <div className='instagram'>
-                  <a href="#">
-                    <FontAwesomeIcon icon={faInstagram}  />
-                  </a>
-                </div>
-                <div className='Email'>
-                  <a href="#">
-                    <FontAwesomeIcon icon={faEnvelope}  />
-                  </a>
-                </div>
-
-
-
-              </div>
-
-            </div>
-            <div className='cards'>
-              <div className='tic-tac'>
-                <img src="../Images/anas_belabbes.jpg" width={200} alt="" />
-              </div>
-              <div className='info-said'>
-                <h5>Anas BELABBES</h5>
-                <p>MVP Microsoft</p>
-              </div>
-
-
-              <div className='nav-Social'>
-                <div className='facebook'>
-                  <a href="https://www.facebook.com/anas.belabbes">
-                    <FontAwesomeIcon icon={faFacebook}  />
-                  </a>
-                </div>
-                <div className='linkedin'>
-                  <a href="https://www.linkedin.com/in/anasbelabbes/">
-                    <FontAwesomeIcon icon={faLinkedin}  />
-                  </a>
-                </div>
-                <div className='instagram'>
-                  <a href="https://www.facebook.com/saidwahidmaroc">
-                    <FontAwesomeIcon icon={faInstagram}  />
-                  </a>
-                </div>
-                <div className='Email'>
-                  <a href="#">
-                    <FontAwesomeIcon icon={faEnvelope}  />
-                  </a>
-                </div>
-
-
-
-              </div>
-
-            </div>
+            {speakersData.map((speaker, index) => (
+              <Speaker key={index} {...speaker} />
+            ))}
           </div>
         </div>
       </section>
