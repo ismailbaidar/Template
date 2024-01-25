d1e8c187586bc81928c078b1f212465bcf2c1191
Mustapha Roudani <roudani.mustapha0@gmail.com>
2024-01-25 09:58:24 +0100

    commit for pull

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,7 +1,21 @@
-export default function Event(){
-    return(
-        <>
-        <h1>Events </h1>
-        </>
-    )
+import React from 'react';
+import '../assets/styles/event.css';
+
+
+function Event() {
+return (
+    <>
+    <div className='section'>
+        <div>
+            <span>Our Event Page (i'll add some design here)</span>
+        </div>
+        <div className='card'>
+
+        </div>
+
+    </div>
+    </>
+)
 }
+
+export default Event;
